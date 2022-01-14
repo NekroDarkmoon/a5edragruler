1c1e4c2edf81e8dd7dca72e17d0782ad5634339e
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-01-14 03:00:36 -0800

    Init

--- scripts/module.js
+++ scripts/module.js
@@ -1,3 +1,11 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                    Imports
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                      Hooks
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Hooks.once('init', async function() {
 
 });
@@ -5,3 +13,35 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 
 });
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                 Extender Class
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Hooks.once('dragRuler.ready', (SpeedProvider) => {
+  class A5ESpeedProvider extends SpeedProvider {
+    get colors ( ) {
+      return [
+        {id: "Walk", default: 0x3222C7},
+        {id: "ActionDash", default: 0xFFEC07},
+        {id: "BonusDash", default: 0xC033E0},
+
+      ];
+    }
+
+    getRanges ( token ) {
+      const movement = token.actor.data.data.attributes.movement;
+      const baseSpeed = Math.max(...Object.values(movement));
+      console.log(baseSpeed);
+      return [];
+    }
+  }
+
+  // Register SpeedProvider
+  dragRuler.registerModule('a5edragruler', A5ESpeedProvider);
+});
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                                 Helper Functions
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
