3728e37d3c00ac02b0b4ac2bc61e6940d31b2ad6
NekroDarkmoon <nekrodarkmoon@gmail.com>
2022-01-14 03:43:05 -0800

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A5E DragRuler Integration
+
+Integration of the Drag Ruler module (https://github.com/manuelVo/foundryvtt-drag-ruler) for A5E. The integration provides support for base movement speed, action dash, and bonus action dash. The base movement speed is set to the maximum of any available speeds. 
--- scripts/module.js
+++ scripts/module.js
@@ -2,20 +2,6 @@
 //                                    Imports
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//                                      Hooks
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Hooks.once('init', async function() {
-
-});
-
-Hooks.once('ready', async function() {
-
-});
-
-
-
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //                                 Extender Class
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -23,25 +9,26 @@ Hooks.once('dragRuler.ready', (SpeedProvider) => {
   class A5ESpeedProvider extends SpeedProvider {
     get colors ( ) {
       return [
-        {id: "Walk", default: 0x3222C7},
-        {id: "ActionDash", default: 0xFFEC07},
-        {id: "BonusDash", default: 0xC033E0},
-
+        {id: "Walk", default: 0x99c140},
+        {id: "ActionDash", default: 0xe7b416},
+        {id: "BonusDash", default: 0xf77926},
       ];
     }
 
     getRanges ( token ) {
       const movement = token.actor.data.data.attributes.movement;
       const baseSpeed = Math.max(...Object.values(movement));
-      console.log(baseSpeed);
-      return [];
+
+      const ranges = [
+        {range: baseSpeed, color: 'Walk'},
+        {range: baseSpeed * 2, color: 'ActionDash'},
+        {range: baseSpeed * 3, color: 'BonusDash'}
+      ];
+
+      return ranges;
     }
   }
 
   // Register SpeedProvider
   dragRuler.registerModule('a5edragruler', A5ESpeedProvider);
 });
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//                                 Helper Functions
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
