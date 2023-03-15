ebbb9e3dae7c5fc35719f49a56c34b463aea5498
JoshLaunchcode <holo.sf0@gmail.com>
2023-03-15 18:05:35 -0500

    updates basic functionality for drag ruler to be compatibile with new V9 data structure

--- scripts/module.js
+++ scripts/module.js
@@ -4,22 +4,29 @@
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Hooks.once('dragRuler.ready', (SpeedProvider) => {
   class A5ESpeedProvider extends SpeedProvider {
-    get colors ( ) {
+    get colors() {
       return [
-        {id: "Walk", default: 0x99c140},
-        {id: "ActionDash", default: 0xe7b416},
-        {id: "BonusDash", default: 0xf77926},
+        { id: "Walk", default: 0x99c140 },
+        { id: "ActionDash", default: 0xe7b416 },
+        { id: "BonusDash", default: 0xf77926 },
       ];
     }
 
-    getRanges ( token ) {
-      const movement = token.actor.system.attributes.movement;
-      const baseSpeed = Math.max(...Object.values(movement));
+    getRanges(token) {
+      const movementValues = []
+
+      Object.entries(token.actor.system.attributes.movement).forEach(([mode, { distance }]) => {
+        if (mode !== "traits") {
+          movementValues.push(distance)
+        }
+      })
+
+      const baseSpeed = Math.max(...movementValues);
 
       const ranges = [
-        {range: baseSpeed, color: 'Walk'},
-        {range: baseSpeed * 2, color: 'ActionDash'},
-        {range: baseSpeed * 4, color: 'BonusDash'}
+        { range: baseSpeed, color: 'Walk' },
+        { range: baseSpeed * 2, color: 'ActionDash' },
+        { range: baseSpeed * 4, color: 'BonusDash' }
       ];
 
       return ranges;
